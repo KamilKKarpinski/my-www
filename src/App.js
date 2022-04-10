9171811d8d7f1b1a4f33da0b5be85bb5841df97e
Kamil Karpinski <kamilkkarp@gmail.com>
2022-04-10 19:03:29 +0200

    CU-2k0j0v1 - Widok z under construction

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Kamil Karpinski
+          Kamil Karpinski Under Construction
         </p>
         <a
           className="App-link"
